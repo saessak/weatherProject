02cadf4d0fdc7b5d638749d1b3843b413d2eeab9
saessak <gkstotkr1201@gmail.com>
2022-07-25 17:36:19 +0900

    second commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
 import { useEffect, useState } from 'react';
+import WeatherBox from './components/WeatherBox';
+import WeatherButton from './components/WeatherButton';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
+  const [weather, setWeather] = useState(null);
 
-  const getCurrentLocation=()=>{
+  // 현재 위치 URL : https://www.w3schools.com/html/html5_geolocation.asp
+  const getCurrentLocation=()=>{ 
     navigator.geolocation.getCurrentPosition((position)=>{
       let lat = position.coords.latitude
       let lon = position.coords.longitude
-      console.log("현재위치", lat, lon)
+      getWeatherByCurrentLocation(lat,lon); //getWeatherByCurrentLocation함수에 lat,lon 함수 넘겨주기
     });
+  }
+
+  const getWeatherByCurrentLocation = async(lat,lon) =>{
+    // 날씨 aip URL : https://openweathermap.org/current
+    let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=6c78c13ab292a52960758b0194c09d09`
+    let response = await fetch(url)
+    let data = await response.json();
+    setWeather(data) //데이터를 weather저장소에 넣는다.
   }
 
   
@@ -17,7 +30,8 @@ function App() {
   },[])
   return (
     <div>
-      현재위치 가져오기
+      <WeatherBox />
+      <WeatherButton />
     </div>
   );
 }
---
+++ src/components/WeatherButton.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Button } from 'react-bootstrap';
+
+const WeatherButton = () => {
+  return (
+    <div>WeatherButton</div>
+  )
+}
+
+export default WeatherButton
